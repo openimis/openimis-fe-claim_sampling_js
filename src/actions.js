6e0f5beb0f31a8f525381316ec0ff5f6adc52b52
sunil <sunilparajuli2002@gmail.com>
2026-07-14 16:13:07 +0545

    feat(OP-3096): add feature to show batch item with sample claim items

--- src/actions.js
+++ src/actions.js
@@ -19,7 +19,7 @@ const formatFilters = ({ percentage, taskGroup, filters }) => {
       .map((f) => {
         const key = f[0];
         let value = JSON.parse(f[1]);
-  
+
         // Decode base64 if it's an encoded UUID
         if (typeof value === 'string' && /^[A-Za-z0-9+/]{22}==$/.test(value)) {
           value = decodeId(value);
@@ -60,13 +60,13 @@ export function fetchSampledClaims(variables) {
     `
       query (
         $claimSample_Id: UUID!,
-        ${variables.after ? ',$after: String' : ''} 
+        ${variables.after ? ',$after: String' : ''}
         ${variables.before ? ',$before: String' : ''}
         ${variables.pageSize ? ',$pageSize: Int' : ''}
         ${variables.status ? ', $status: String' : ''}
          ) {
         samplingBatchClaims(
-          claimSamplingId: $claimSample_Id, 
+          claimSamplingId: $claimSample_Id,
           ${variables.before ? ',before:$before, last:$pageSize' : ''}
           ${!variables.before ? ',first:$pageSize' : ''}
           ${variables.after ? ',after:$after' : ''}
@@ -87,6 +87,40 @@ export function fetchSampledClaims(variables) {
     `,
     variables,
     ACTION_TYPE.GET_CLAIMS_FOR_SAMPLING,
+  );
+}
+
+
+export function fetchAllSampledClaims(variables) {
+  return graphqlWithVariables(
+    `
+      query (
+        $claimSample_Id: UUID!,
+        ${variables.after ? ',$after: String' : ''}
+        ${variables.before ? ',$before: String' : ''}
+        ${variables.pageSize ? ',$pageSize: Int' : ''}
+         ) {
+        samplingBatchClaims(
+          claimSamplingId: $claimSample_Id,
+          ${variables.before ? ',before:$before, last:$pageSize' : ''}
+          ${!variables.before ? ',first:$pageSize' : ''}
+          ${variables.after ? ',after:$after' : ''}
+        )
+        {
+          totalCount
+          pageInfo { hasNextPage, hasPreviousPage, startCursor, endCursor}
+          edges
+          {
+            node
+            {
+              id, uuid, jsonExt, code, insuree { otherNames, lastName }, healthFacility { code }, dateClaimed, admin { otherNames, lastName }, status, reviewStatus
+            }
+          }
+        }
+      }
+    `,
+    variables,
+    ACTION_TYPE.GET_ALL_CLAIMS_FOR_SAMPLING,
   );
 }
 
@@ -127,14 +161,14 @@ export function resolveClaimTask(task, clientMutationLabel, user, approveOrFail,
   const userId = user?.id;
 
   const mutation2 = prepareMutation(
-    `mutation ($clientMutationLabel:String, $clientMutationId: String, $id:UUID!, 
+    `mutation ($clientMutationLabel:String, $clientMutationId: String, $id:UUID!,
       $businessStatus: JSONString!, ${additionalData ? '$additionalData: JSONString!' : ''}
     ) {
       resolveTask(
       input: {
         clientMutationId: $clientMutationId
         clientMutationLabel: $clientMutationLabel
-  
+
         id: $id
         businessStatus: $businessStatus
         ${additionalData ? 'additionalData: $additionalData' : ''}
